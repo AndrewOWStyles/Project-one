e8c83b151f4cd3ef464409c4858f2444fbd87cc9
Andrew Styles <andystyles@hotmail.co.uk>
2023-02-09 18:51:00 +0000

    fixed carousel buttons, hide and show div on button click

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
 let searchBtn = document.querySelector("#search-button")
 let searchInput = document.getElementById("search-text");
-const searchBox = document.getElementById("imgText");
-let searchTerm = searchBox.value;
 let jobsArea = document.getElementById("princeColor");
 const url = "https://pixabay.com/api/?key=";
 const apiKey = "33299072-c6d09cbcb29cf3d5fcf4e9d15&q=";
 let apiKeyJobs = "99a4e0e7&app_key=854849cfbeff94c09ce8edea5ae2ea2a";
 const imageType = "&image_type=photo";
 const imageDiv = document.querySelector("#imageArea");
+let imgInput = document.querySelector("#imgInput");
+const imgBtn = document.querySelector("#imgBtn");
 let images = document.querySelector("#images");
 let index = 0;
 let imageTimeout;
@@ -17,84 +17,91 @@ let displayCardsArr = [];
 
 // function linked to HTML button. on click take input value in searchTerm and calls fetchImage().
 function imgSearch() {
-  searchTerm = searchBox.value;
-  search = searchInput.value;
+  searchTerm = searchInput.value;
   index = 0;
   fetchImage();
-  // carousel buttons load into the page as display: none, this targets the buttons through a loop and sets the to visible.
+  let imgDiv = document.querySelector("#imgDiv")
+  imgDiv.style.visibility = "visible";
+  // carousel buttons load into the page as display: none, this targets the buttons through a loop and sets the to visible. 
   let caraBtn = document.getElementsByClassName("carouselBtn");
   for (var i = 0; i < caraBtn.length; i++) {
-    caraBtn[i].style.display = "inline-block";
+    caraBtn[i].style.visibility = "visible";
   }
-
+}
+  
+function secondImgSearch() {
+  searchTerm = imgInput.value;
+  index = 0;
+  fetchImage();
+  imgInput.value = ""
 }
 
 // image fetch functions, makes a request to the api by combining the 4 variables to make the url and passes the response to the createImage function.
 function fetchImage() {
   let queryURL = url + apiKey + searchTerm + imageType;
   fetch(queryURL)
-    .then((response) => response.json())
-    .then((data) => {
-      // console.log(queryURL);
-      // console.log(data);
-      createImage(data.hits);
+  .then((response) => response.json())
+  .then((data) => {
+    // console.log(queryURL);
+    // console.log(data);
+    hits = data.hits
+    createImage(hits);
     });
-}
-
-// uses data from the promise to display the image from the api. 
-function createImage(hits) {
-  images.innerHTML = "";
-  let img = document.createElement("img");
-  // first image is the first one from the returned array.
-  img.src = hits[index].webformatURL;
-  // img class added to the generated image and images appended to the imageDiv.
-  img.classList.add("img");
-  images.appendChild(img);
-  imageDiv.append(images);
-
-  // button to go to the next item in an array. clears timeout using imageTimeout.
-  nextBtn.addEventListener("click", function () {
-    clearTimeout(imageTimeout);
-    // if the index is equal to the last item in the array it resets the array to the first item, if not it goes to the next item.
-    if (index === hits.length - 1) {
-      index = 0;
-    } else {
-      index++;
-    }
+  }
+    
+    // uses data from the promise to display the image from the api. 
+    function createImage(hits) {
+      images.innerHTML = "";
+      let img = document.createElement("img");
+      // first image is the first one from the returned array.
+      img.src = hits[index].webformatURL;
+      // img class added to the generated image and images appended to the imageDiv.
+      img.classList.add("img");
+      images.appendChild(img);
+      imageDiv.append(images);
+      
+      // button to go to the next item in an array. clears timeout using imageTimeout.
+      nextBtn.addEventListener("click", function () {
+        clearTimeout(imageTimeout);
+        // if the index is equal to the last item in the array it resets the array to the first item, if not it goes to the next item.
+        if (index === hits.length - 1) {
+          index = 0;
+        } else {
+          index++;
+        }
+        imageTimeout = setTimeout(() => createImage(hits), 500);
+      });
+      
+      // button to go to a previous item in an array. clears timeout using imageTimeout.
+      prevBtn.addEventListener("click", function () {
+        clearTimeout(imageTimeout);
+        // if the index is 0 it sets the current item to the last of the array, if not it goes to the previous item.
+        if (index === 0) {
+          index = hits.length - 1;
+        } else {
+          index--;
+        }
     imageTimeout = setTimeout(() => createImage(hits), 500);
   });
 }
-
-// button to go to a previous item in an array. clears timeout using imageTimeout.
-prevBtn.addEventListener("click", function () {
-  clearTimeout(imageTimeout);
-  // if the index is 0 it sets the current item to the last of the array, if not it goes to the previous item.
-  if (index === 0) {
-    index = hits.length - 1;
-  } else {
-    index--;
-  }
-  imageTimeout = setTimeout(() => createImage(hits), 500);
-});
-
-//Display company name from the first object of the search results
-let searchQuerryUrl = "https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=36e59f90&app_key=70ad6f78f2c44754114265af2caed74f";
-let myJobResult = fetch(searchQuerryUrl)
+  
+  //Display company name from the first object of the search results
+  let searchQuerryUrl = "https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=36e59f90&app_key=70ad6f78f2c44754114265af2caed74f";
+  let myJobResult = fetch(searchQuerryUrl)
   .then((response) => response.json())
   .then(jobResult => {
     // console.log(jobResult.results[0])
-
+    
     let companyName = jobResult.results[0].company.display_name
     // console.log(companyName)
     let { display_name } = jobResult.results[0].location;
     // console.log(display_name)
-
-
+    
+    
     let resultElement = document.createElement("div");
     resultElement.textContent = JSON.stringify(jobResult.results[0].company.display_name);
-
+    
     document.getElementById("princeColor").append(resultElement);
-
 
   })
 
@@ -125,7 +132,6 @@ function fetchResults() {
     .then((response) => response.json())
     .then(jobResult => {
 
-
       // console.log(jobResult.results)
 
       //Loop through the results array to retrieve the information for display 
@@ -141,7 +147,6 @@ function fetchResults() {
         const { area } = element.location;
 
         // console.log(title, label, display_name, contract_time, contract_type, salary_min, salary_max, description, created, area);
-
 
         //Object to be used for sorting the array prior to display
         // let obj = {
@@ -181,11 +186,9 @@ function fetchResults() {
 
         // }
 
-
       }
     })
 };
-
 
 //
 // console.log(displayCardsArr);
@@ -210,7 +213,6 @@ filterBtns.addEventListener("click", (event) => {
 // Function to reset search criteria
 function clearSearch() {
   jobsArea.innerHTML = "";
-  imageDiv.innerHTML = "";
 }
 
 //Create an event listener for the search button
@@ -225,9 +227,8 @@ searchBtn.addEventListener("click", function (event) {
   //Retrieve job search results
   fetchResults();
   //Clear the user input on submit
-  // searchInput.value = '';
+  searchInput.value = '';
 })
-
 
 //Create an event listener for the enter key
 searchInput.addEventListener("keypress", function (event) {
@@ -236,7 +237,7 @@ searchInput.addEventListener("keypress", function (event) {
     clearSearch()
     //Uses the job search button to submit user input
     searchBtn.click();
-    // searchInput.value = '';
+    searchInput.value = '';
   }
 
 })
